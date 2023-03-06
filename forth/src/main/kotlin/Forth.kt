30f39d10f6ba21f82fb6ad39bf0b8341b243b3fe
victor3099 <s301607@studenti.polito.it>
2023-03-06 11:55:55 +0100

    refactoring code

--- forth/src/main/kotlin/Forth.kt
+++ forth/src/main/kotlin/Forth.kt
@@ -2,17 +2,32 @@ import java.lang.Exception
 
 class Forth {
 
-    fun division(op1: Int, op2: Int): Int {
-        if(op2 == 0) {
-            throw Exception("divide by zero")
+    private var parametersPassed: ArrayDeque<String> = ArrayDeque()
+    private val userDefinedFunction by lazy {
+        val userDefinedFunction = mutableMapOf<String?, ArrayDeque<String>>()
+        for(x in parametersPassed) {
+
+            val definedWords = ArrayDeque(x.trim().split("\\s+".toRegex()).filter { it != ":" && it != ";" })
+            val wordDefined = definedWords.removeFirst()
+
+            for((i, e) in definedWords.withIndex()) {
+                if(userDefinedFunction.contains(e)) {
+                    definedWords[i] = userDefinedFunction[e]!!.joinToString(" ")
+                }
+            }
+            userDefinedFunction[wordDefined.lowercase()] = definedWords
         }
-        return op1/op2
+        userDefinedFunction
     }
+    private fun checkListParam(parameters: List<String>): ArrayDeque<Int> {
 
-
-    fun checkListParam(parameters: List<String>): Boolean {
         val stackOperands = ArrayDeque(parameters.filter { it.all { c -> c.isDigit() } }.toList().map { it.toInt() })
         val operatorsFun = parameters.filter{ !it.all { c -> c.isDigit() } }.map { it.lowercase() }.toList()
+
+        if(parameters.first() == ":") {
+            throw Exception("illegal operation")
+        }
+
         for(x in operatorsFun) {
             if(!checkDefaultFunction(x)) {
                 throw Exception("undefined operation")
@@ -20,7 +35,7 @@ class Forth {
         }
 
         if(operatorsFun.isEmpty()) {
-            return true
+            return ArrayDeque(parameters.filter { it.all { c -> c.isDigit() } }.toList().map { it.toInt() })
         }
 
         if(stackOperands.size == 1 && !(operatorsFun.contains("dup") || operatorsFun.contains("drop"))) {
@@ -29,10 +44,9 @@ class Forth {
         if(stackOperands.isEmpty()) {
             throw Exception("empty stack")
         }
-        return false
+        return ArrayDeque()
     }
-
-    fun checkDefaultFunction(operation: String): Boolean {
+    private fun checkDefaultFunction(operation: String): Boolean {
         return when(operation.lowercase()) {
             "+" -> true
             "-" -> true
@@ -45,50 +59,66 @@ class Forth {
             else -> false
         }
     }
+    private fun evaluateOperands(parametersPassed: ArrayDeque<String>): List<String> {
+        val tmpValue = parametersPassed
+            .removeLast()
+            .split("\\s+".toRegex())
+            .toList()
+            .map { it.lowercase()  }
 
-    fun evaluate(vararg line: String): List<Int> {
-
-        // Take the first element of Sequence vararg
-        //var operands = line.last().split("\\s+".toRegex()).toList()
-        var parametersPassed = ArrayDeque(line.toList())
-        var operands = parametersPassed.removeLast().split("\\s+".toRegex()).toList()
-        operands = operands.map { it.lowercase() }
-        var userDefinedFunction = mutableMapOf<String?, ArrayDeque<String>>()
-
-        for(x in parametersPassed) {
-
-            var definedWords = ArrayDeque(x.replace(":", "").replace(";", "").trim().split("\\s+".toRegex()))
-            var wordDefined = definedWords.removeFirst()
-
-            for((i, e) in definedWords.withIndex()) {
-                if(userDefinedFunction.contains(e)) {
-                    definedWords.set(i, userDefinedFunction.get(e)!!.joinToString(" "))
-                }
-            }
-
-            userDefinedFunction.put(wordDefined.lowercase(), definedWords)
-
-        }
-
-        if(operands.first() == ":") {
-            throw Exception("illegal operation")
-        }
-
-        var finalOperands = ArrayDeque<String>()
-        for(x in operands) {
+        val finalOperands = ArrayDeque<String>()
+        for(x in tmpValue) {
             if(userDefinedFunction.contains(x)) {
-                for(i in userDefinedFunction.get(x)!!.iterator()) {
+                for(i in userDefinedFunction[x]!!.iterator()) {
                     finalOperands.add(i.lowercase())
                 }
             } else {
                 finalOperands.add(x.lowercase())
             }
         }
-        operands = finalOperands
-        println(operands)
-        val checking = checkListParam(operands)
-        if(checking) { return ArrayDeque(operands.filter { it.all { c -> c.isDigit() } }.toList().map { it.toInt() })}
+        return finalOperands
+    }
+    private fun duplicate(res: ArrayDeque<Int>) = res.add(res.last())
+    private fun dropping(res: ArrayDeque<Int>) =  res.removeLast()
+    private fun over(res: ArrayDeque<Int>) = res.addLast(res[res.size-2])
+    private fun swapping(res: ArrayDeque<Int>) {
+        val v1 = res.removeLast()
+        val v2 = res.removeLast()
+        res.addLast(v1)
+        res.addLast(v2)
+    }
+    private fun plus(res: ArrayDeque<Int>) {
+        val v1 = res.removeLast()
+        val v2 = res.removeLast()
+        res.addLast(v1+v2)
+    }
+    private fun minus(res: ArrayDeque<Int>) {
+        val v1 = res.removeLast()
+        val v2 = res.removeLast()
+        res.addLast(v2-v1)
+    }
+    private fun mul(res: ArrayDeque<Int>) {
+        val v1 = res.removeLast()
+        val v2 = res.removeLast()
+        res.addLast(v1*v2)
+    }
+    private fun division(res: ArrayDeque<Int>) {
+        val v2 = res.removeLast()
+        val v1 = res.removeLast()
+        if(v2 == 0) {
+            throw Exception("divide by zero")
+        }
+        res.addLast(v1/v2)
+    }
+    fun evaluate(vararg line: String): List<Int> {
 
+        this.parametersPassed.addAll(line.toList())
+        val operands = evaluateOperands(this.parametersPassed)
+
+        val checkingList = checkListParam(operands)
+        if(!checkingList.isEmpty()) {
+            return checkingList
+        }
 
         val res = ArrayDeque<Int>()
         for(x in operands) {
@@ -96,41 +126,14 @@ class Forth {
                 res.add(x.toInt())
             } else {
                 when(x.lowercase()) {
-                    "dup" -> {
-                        res.add(res.last())
-                    }
-                    "drop" -> {
-                        res.removeLast()
-                    }
-                    "swap" -> {
-                        val v1 = res.removeLast()
-                        val v2 = res.removeLast()
-                        res.addLast(v1)
-                        res.addLast(v2)
-                    }
-                    "over" -> {
-                        res.addLast(res.get(res.size-2))
-                    }
-                    "+" -> {
-                        val v1 = res.removeLast()
-                        val v2 = res.removeLast()
-                        res.addLast(v1+v2)
-                    }
-                    "-" -> {
-                        val v1 = res.removeLast()
-                        val v2 = res.removeLast()
-                        res.addLast(v2-v1)
-                    }
-                    "*" -> {
-                        val v1 = res.removeLast()
-                        val v2 = res.removeLast()
-                        res.addLast(v1*v2)
-                    }
-                    "/" -> {
-                        val v1 = res.removeLast()
-                        val v2 = res.removeLast()
-                        res.addLast(division(v2, v1))
-                    }
+                    "dup" -> duplicate(res)
+                    "drop" -> dropping(res)
+                    "swap" -> swapping(res)
+                    "over" -> over(res)
+                    "+" -> plus(res)
+                    "-" -> minus(res)
+                    "*" -> mul(res)
+                    "/" -> division(res)
                     else -> {
                         break
                     }
